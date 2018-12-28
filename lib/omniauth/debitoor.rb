6899e52c87006f1edd7a397cbe6995b7d1ec32d8
Alessio Santo <alessio.santocs@gmail.com>
2018-12-29 00:31:28 +0800

    Initial commit

---
+++ lib/omniauth/debitoor.rb
@@ -0,0 +1,7 @@
+require "omniauth/debitoor/version"
+
+module Omniauth
+  module Debitoor
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/debitoor/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Debitoor
+    VERSION = "0.1.0"
+  end
+end
